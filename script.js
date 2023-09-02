fdd8e3ef263eb9e824f3151d8b5c6a2a95c848dc
Andre Pacola <andreluizzpacolla@outlook.com>
2023-09-02 20:50:34 -0300

    first commit newproject

---
+++ script.js
@@ -0,0 +1,55 @@
+const inputTask = document.querySelector(".input-task");
+const buttonTask = document.querySelector(".button-adc-task");
+const ul = document.querySelector(".list-task");
+
+let myList = [];
+
+function getTask() {
+  if (inputTask.value.length) {
+    myList.push({
+      task: inputTask.value,
+      complete: false,
+    });
+  } else {
+    alert("ATENÇÃO Campo vazio.. Adicione uma tarefa ! ");
+  }
+
+  inputTask.value = "";
+  showTask();
+}
+
+function showTask() {
+  let newList = "";
+
+  myList.forEach((value, index) => {
+    newList += `
+    <li class="task ${value.complete && "done"}">
+      <img class='checked' src="assets/checked.png" alt="checked" onclick ='completeTask(${index})'/>
+      <p>${value.task}</p>
+      <img class='trash' src="assets/trash.png" alt="trash" onclick ='deleteTask(${index})'/>
+    </li>`;
+  });
+
+  ul.innerHTML = `${newList}`;
+  localStorage.setItem("lista", JSON.stringify(myList));
+}
+
+function completeTask(i) {
+  myList[i].complete = !myList[i].complete;
+  showTask();
+}
+
+function deleteTask(i) {
+  myList.splice(i, 1);
+  showTask();
+}
+
+function reloadTask() {
+  const tasklocalStorage = localStorage.getItem("lista");
+  myList = JSON.parse(tasklocalStorage);
+  showTask();
+}
+
+reloadTask();
+
+buttonTask.addEventListener("click", getTask);
